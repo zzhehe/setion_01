d0d4ec86bb7b344fe68f22e2cbf9ee9ac2f2007f
zzhehe <460475523@qq.com>
2018-02-04 13:32:11 +0800

    Initial commit

---
+++ Source/setion_01/setion_01GameModeBase.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "setion_01GameModeBase.h"
+
+
+
+
---
+++ Source/setion_01/setion_01GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "setion_01GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SETION_01_API Asetion_01GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
